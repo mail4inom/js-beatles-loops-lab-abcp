b77cea0a1c139ffb8ad04bfa425f5e258bebe4b3
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-01-24 20:02:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for(var i = 0; i < musicians.length; i++){
+array.push(musicians[i]+ " plays " +instruments[i]);
+  }
+  return array;
+}
+var facts = [
+ "He was the last Beatle to learn to drive",
+ "He was never a vegetarian",
+ "He was a choir boy and boy scout",
+ "He hated the sound of his own voice"
+];
+function johnLennonFacts(array){
+var result = [];
+var i = 0;
+  while(i < array.length){
+    result.push(array[i]+ "!!!");
+    i++;
+  }
+  return result;
+}
+function iLoveTheBeatles(n){
+var arr = [];
+do{
+  arr.push('I love the Beatles!');
+  n++;
+} while(n < 15)
+return arr;
+}
