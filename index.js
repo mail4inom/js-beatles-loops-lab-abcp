62ae87eade68d2f34b1e8d8b3f22747a5839fc5a
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-01-30 23:43:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
+  for(var i = 0; i < musicians.length; i++){
+    console.log(array.`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
 }
